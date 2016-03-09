d1e4ba414188c197702dbff7064a988efed88d75
sparklyballs <sparklyballs@gmail.com>
2016-03-09 11:01:13 +0000

    add custom cnf

--- init/30_config.sh
+++ init/30_config.sh
@@ -11,5 +11,10 @@ sed -i -e 's/\(user.*=\).*/\1 abc/g' /etc/mysql/my.cnf
 sed -i -e "s#\(datadir.*=\).*#\1 $DATADIR#g" /etc/mysql/my.cnf
 sed -i "s/user='mysql'/user='abc'/g" /usr/bin/mysqld_safe
 
+# setup custom cnf file
+[[ ! -f /config/custom.cnf ]] && cp /defaults/my.cnf /config/custom.cnf
+[[ ! -L /etc/mysql/conf.d/custom.cnf && -f /etc/mysql/conf.d/custom.cnf ]] && rm /etc/mysql/conf.d/custom.cnf
+[[ ! -L /etc/mysql/conf.d/custom.cnf ]] && ln -s /config/custom.cnf /etc/mysql/conf.d/custom.cnf
+
 # set permissions
 chmod -R 777 /var/run/mysqld
